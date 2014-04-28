97ed6e53f9c7d32f4e56b105fbbbc09145239f0b
alim4 <theineffablebob@gmail.com>
2014-04-27 23:27:17 -0700

    Fixed conversion functions

--- Solitaire.py
+++ Solitaire.py
@@ -11,6 +11,9 @@ __author__ = 'anthonylim'
 
 def main():
     deck = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12'
+    result = []
+
+    print convert_val_to_char(53)
 
     flag = get_input()
     while flag != 'e' and flag != 'd':
@@ -20,9 +23,11 @@ def main():
     deck = raw_input("What is the initial deck? ")
 
     if flag == 'e':
-        encrypt_message(message, deck)
+        result = encrypt_message(message, deck)
     if flag == 'd':
-        decrypt_message(message, deck)
+        result = decrypt_message(message, deck)
+
+    print result
 
     return 0
 
@@ -39,10 +44,20 @@ def get_input():
 
     return flag
 
-def encrypt_message (msg, deck):
-    return
+def encrypt_message(msg, deck):
+    result = []
+    for i in range(msg.__len__()):
+        if msg[i].isspace() or msg[i].isdigit():
+            continue
+        aa = convert_char_to_val(msg[i])
+        bb = convert_char_to_val(deck[i])
+        print ("aa: {0} | bb: {1}".format(aa, bb))
+        temp = convert_val_to_char((convert_char_to_val(msg[i]) + convert_char_to_val(deck[i]) % 52))
+        result.append(temp)
 
-def decrypt_message (msg, deck):
+    return result
+
+def decrypt_message(msg, deck):
     return
 
 # Convert a CHARACTER to an ASCII INTEGER
@@ -59,11 +74,13 @@ def convert_char_to_val(char):
 # Convert an ASCII INTEGER value to a CHARACTER
 def convert_val_to_char(val):
     base = 'a'
-    v = ''
-    if val < 27:
-        v = chr(val + ord(base) - 1)
+    deck = 52
+
+    if 26 < val <= 52:
+        v = chr((val % 53) + ord('a') - 59)
     else:
-        v = chr(val + ord(base) - 59)
+        v = chr((val % 52) + ord('a') - 1)
+
 
     return v
 
